d30794123fe8af6434c7502cdc410d644b65b672
TrueZhuanjia <veimin@foxmail.com>
2025-07-25 09:27:33 +0800

    fix osw tool outbounds issue again

--- VoidLink/Input/OnScreenWidgetView.swift
+++ VoidLink/Input/OnScreenWidgetView.swift
@@ -1138,8 +1138,8 @@ import UIKit
         let offsetX = currentLocation.x - latestTouchLocation.x;
         let offsetY = currentLocation.y - latestTouchLocation.y;
         
-        let outOfBoundsX = center.x+offsetX > (self.superview?.bounds.width)! || center.x+offsetX < 0
-        let outOfBoundsY = center.y+offsetY > (self.superview?.bounds.height)! || center.y+offsetY < 0
+        let outOfBoundsX = center.x+offsetX >= (self.superview?.bounds.width)! || center.x+offsetX < 0
+        let outOfBoundsY = center.y+offsetY >= (self.superview?.bounds.height)! || center.y+offsetY < 0
 
         center = CGPoint(x: outOfBoundsX ? center.x : center.x+offsetX, y: outOfBoundsY ? center.y : center.y+offsetY)
         
