fd8abccc2dfd8a2be07c63b722e27fe93214088d
TrueZhuanjia <veimin@foxmail.com>
2025-10-22 11:43:26 +0800

    Fixed: widget profile, widget tool entry doesn't work for single point touch

--- VoidLink/ViewControllers/StreamFrameViewController.m
+++ VoidLink/ViewControllers/StreamFrameViewController.m
@@ -179,7 +179,7 @@
 
 
 - (bool)isOscLayoutToolEnabled{
-    return (_settings.touchMode.intValue == RelativeTouch || _settings.touchMode.intValue == NativeTouch || _settings.touchMode.intValue == NativeTouch || _settings.touchMode.intValue == TouchDisabled) && _settings.onscreenControls.intValue == OnScreenControlsLevelCustom;
+    return (_settings.touchMode.intValue == RelativeTouch || _settings.touchMode.intValue == NativeTouch || _settings.touchMode.intValue == AbsoluteTouch || _settings.touchMode.intValue == TouchDisabled) && _settings.onscreenControls.intValue == OnScreenControlsLevelCustom;
 }
 
 - (void)setupPiPControllerWithRenderer:(VideoDecoderRenderer *)videoRenderer {    // Ensure we have the renderer and its layer
